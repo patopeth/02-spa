d45e1da421ae31f643a5e7f25dfb51083d49e8b3
patopeth <Plussenhoff@nosis.interno>
2019-01-11 17:07:59 -0300

    Cambios en heroes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent} from './components/about/about.component';
+import { HeroesComponent} from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+const app_routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const app_routing = RouterModule.forRoot(app_routes);
+
+// ng2 y luego routes por el snippet
